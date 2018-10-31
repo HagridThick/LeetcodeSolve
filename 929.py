88cd55f89425620fb26ed3437468c80306e56a6f
徐晨鸥 <464154015@qq.com>
2018-10-31 13:20:56 +0800

    python3 solve 929

---
+++ 929.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numUniqueEmails(self, emails):
+        """
+        :type emails: List[str]
+        :rtype: int
+        """
+        unrepeat = set()
+        for email in emails:
+            spl = email.split('@')
+            pre = spl[0].split('+')
+            unrepeat.add(pre[0].replace('.','') + spl[1])
+        return len(unrepeat)
+
